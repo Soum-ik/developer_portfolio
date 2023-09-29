c11965cdc95d64bd8016a67550440cefab9f4e32
soum-ik <sarkarsoumik215@gmail.com>
2023-09-29 19:13:18 +0600

    route

--- src/components/moblie-nav/moblie.jsx
+++ src/components/moblie-nav/moblie.jsx
@@ -12,13 +12,16 @@ export default function Moblie_item() {
   return (
     <div className=" sm:pl-7  sm:pt-3  ">
       <div className="flex items-center justify-center space-x-4 text-cyan-50  text-4xl duration-1000 transition-opacity ">
-        <a  target="_blank" href="https://www.facebook.com/soumik.sarkar.16547">
+        <a target="_blank" href="https://www.facebook.com/soumik.sarkar.16547">
           <BsFacebook className="opacity-30 hover:opacity-75" />
         </a>
-        <a  target="_blank" href="https://github.com/Soum-ik/">
+        <a target="_blank" href="https://github.com/Soum-ik/">
           <BsGithub className="opacity-30 hover:opacity-75" />
         </a>
-        <a  target="_blank" href="https://www.instagram.com/soumik.sarkar.16547/">
+        <a
+          target="_blank"
+          href="https://www.instagram.com/soumik.sarkar.16547/"
+        >
           <FaInstagram className="opacity-30 hover:opacity-75" />
         </a>
       </div>
