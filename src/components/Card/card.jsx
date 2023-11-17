54b01775a398e3feeb7dc0a2fedf47861dc9b162
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2023-11-17 17:18:25 +0600

    feat/add new project

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -5,8 +5,7 @@ import img from "../../img/ss.png";
 import img1 from "../../img/ss1.png";
 import img2 from "../../img/ss3.png";
 import img3 from "../../img/morden.png";
-
-
+import img4 from "../../img/resume.png";
 export default function Card({ btn1, btn2 }) {
   const Contents = [
     {
@@ -40,6 +39,14 @@ export default function Card({ btn1, btn2 }) {
       live: "https://morden-ui.vercel.app/",
       git: "https://github.com/Soum-ik/result_Finder-",
     },
+    {
+      id: 5,
+      Title: "Resume Builder",
+      Dic: "Unlock new possibilities for career advancement with our Resume Builder application, streamlining CV creation in just 3 minutes. Stay tuned for exciting updates as we enhance your experience with additional features!   Revolutionize Your CV Creation! ",
+      Image: img4,
+      live: "https://resume-builder-soum-ik.vercel.app/",
+      git: "https://github.com/Soum-ik/Resume_Builder/",
+    },
   ];
 
   return (
@@ -60,11 +67,11 @@ export default function Card({ btn1, btn2 }) {
             <div className="">
               <div className=" my-3 text-xs flex items-center flex-col sm:flex-row justify-between sm:gap-0 gap-3">
                 {/* some cahnge */}
-                <div className="space-x-3 space-y-2 flex-1 ">
+                <div className="space-x-3 space-y-2 flex-1 text-[15px] ">
                   <Button>{btn1}</Button>
                   <Button>{btn2}</Button>
                 </div>
-                <div className=" text-mainColor/30 flex items-center space-x-3 text-xl">
+                <div className=" text-mainColor/30 flex items-center space-x-3 text-2xl">
                   <a href={content.git} target="_blank">
                     <BsGithub />
                   </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,14 +22,3 @@ export default {
   },
   plugins: [],
 };
-
-// animation: spin 1s linear infinite;
-
-// @keyframes spin {
-//   from {
-//     transform: rotate(0deg);
-//   }
-//   to {
-//     transform: rotate(360deg);
-//   }
-// }
